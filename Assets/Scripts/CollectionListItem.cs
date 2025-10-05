27650fe9d46ce6379f4f4a6898000af2866a435b
Daniel Gold <saganr45@gmail.com>
2025-10-04 21:34:54 -0500

    Collection changes

---
+++ Assets/Scripts/CollectionListItem.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class CollectionListItem : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI cardNameText;
+    [SerializeField] Button addCardButton;
+    [SerializeField] Button removeCardButton;
+    [SerializeField] TextMeshProUGUI cardQuantityText;
+    [SerializeField] CardTemplate cardTemplate;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CollectionUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+public class CollectionUI : MonoBehaviour
+{
+    [SerializeField] RectTransform cardList;
+    [SerializeField] GameObject cardListPrefab;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void loadCards()
+    {
+
+    }
+}
