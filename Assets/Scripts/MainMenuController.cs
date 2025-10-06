2afff031b4b5f96a0d3b3c3bc76ca5265289fe53
Daniel Gold <contact@golddaniel.ca>
2025-10-05 21:14:25 -0500

    Main menu bb

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
 	public RectTransform mainMenuPanel;
 	public RectTransform howToPlayPanel;
- 
+
+	public RectTransform storyPanel;
+
+	public CanvasGroup panel0;
+	public CanvasGroup panel1;
+	public CanvasGroup panel2;
+
 	public void OnPlayPressed()
 	{
 		if (GameProgress.Instance.hasCompletedTutorial)
@@ -22,5 +30,47 @@ public class MainMenuController : MonoBehaviour
 	public void OnTutorialPlayPressed()
 	{
 		SceneManager.LoadScene("GameScene");
+	}
+
+	public void StoryPressed()
+	{
+		StartCoroutine(AnimatePanels());
+	}
+
+	public void OnReturnToMenuPressed()
+	{
+		panel0.alpha = 0;
+		panel1.alpha = 0;
+		panel2.alpha = 0;
+		storyPanel.gameObject.SetActive(false);
+	}
+
+	IEnumerator AnimatePanels()
+	{
+		panel0.alpha = 0;
+		panel1.alpha = 0;
+		panel2.alpha = 0;
+		storyPanel.gameObject.SetActive(true);
+
+		yield return new WaitForSecondsRealtime(0.1f);
+
+		yield return FadeInPanel(panel0);
+		yield return new WaitForSeconds(1f);
+		yield return FadeInPanel(panel1);
+		yield return new WaitForSeconds(1f);
+		yield return FadeInPanel(panel2);
+	}
+
+	IEnumerator FadeInPanel(CanvasGroup panel)
+	{
+		float timer = 0;
+		float time = 2f;
+		while (timer < time)
+		{
+			panel.alpha = Mathf.Pow(timer / time, 2.2f);
+			timer += Time.deltaTime;
+			yield return null;
+		}
+		panel.alpha = 1;
 	}
 }
